b5ca273a5937c0f79bd112821860d0e45e8d775b
KosukeNakane <kd1429179@st.kobedenshi.ac.jp>
2025-08-12 20:07:34 +0900

    🔧 VercelへのDeployに向けた各種設定の追加、変更

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/analyze': {
-        target: 'http://localhost:5001',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const apiBase = env.VITE_API_BASE_URL
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/analyze': {
+          target: apiBase,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 });
 
